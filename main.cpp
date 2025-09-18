30648d3469e9f67cbc56e2c8ab1d1ed0a911546e
ViNN280801 <vladislav.semykin@gmail.com>
2025-09-18 12:04:34 +0300

    style: uncommented example code
    Signed-off-by: ViNN280801 <vladislav.semykin@gmail.com>

--- main.cpp
+++ main.cpp
@@ -91,136 +91,136 @@ main()
     return 1;
   }
 
-//   std::cout << "CoreDumpGenerator initialized successfully!\n";
-//   std::cout << "Dump directory: " << CoreDumpGenerator::getDumpDirectory() << "\n";
-//   std::cout << "Current dump type: " << DumpFactory::getDescription(CoreDumpGenerator::getCurrentDumpType()) << "\n";
+  std::cout << "CoreDumpGenerator initialized successfully!\n";
+  std::cout << "Dump directory: " << CoreDumpGenerator::getDumpDirectory() << "\n";
+  std::cout << "Current dump type: " << DumpFactory::getDescription(CoreDumpGenerator::getCurrentDumpType()) << "\n";
 
-//   // Test admin privilege checking
-//   std::cout << "\n=== Admin Privilege Check ===\n";
-//   bool isAdmin = CoreDumpGenerator::isAdminPrivileges();
-//   std::cout << "Running with admin privileges: " << (isAdmin ? "YES" : "NO") << "\n";
+  // Test admin privilege checking
+  std::cout << "\n=== Admin Privilege Check ===\n";
+  bool isAdmin = CoreDumpGenerator::isAdminPrivileges();
+  std::cout << "Running with admin privileges: " << (isAdmin ? "YES" : "NO") << "\n";
 
-//   // Test logging with different privilege levels
-//   std::cout << "\n=== Logging Test ===\n";
-//   std::cout << "Testing logging with sensitive information:\n";
+  // Test logging with different privilege levels
+  std::cout << "\n=== Logging Test ===\n";
+  std::cout << "Testing logging with sensitive information:\n";
 
-//   // This will show different sanitization based on admin privileges
-//   CoreDumpGenerator::generateDump("Test dump for admin privilege logging demonstration");
+  // This will show different sanitization based on admin privileges
+  CoreDumpGenerator::generateDump("Test dump for admin privilege logging demonstration");
 
-//   std::cout << "\n=== Available Dump Types ===\n";
-//   std::cout << "=== Basic Mini Dumps (64KB) ===\n";
-// #if DUMP_CREATOR_WINDOWS
-//   std::cout << "1. MINI_DUMP_NORMAL\n";
-//   std::cout << "2. MINI_DUMP_WITHOUT_OPTIONAL_DATA\n";
-//   std::cout << "3. MINI_DUMP_IGNORE_INACCESSIBLE_MEMORY\n";
-//   std::cout << "4. MINI_DUMP_FILTER_MEMORY\n";
-//   std::cout << "5. MINI_DUMP_SCAN_MEMORY\n";
-//   std::cout << "6. MINI_DUMP_FILTER_MODULE_PATHS\n";
-//   std::cout << "7. MINI_DUMP_WITHOUT_AUXILIARY_STATE\n";
+  std::cout << "\n=== Available Dump Types ===\n";
+  std::cout << "=== Basic Mini Dumps (64KB) ===\n";
+#if DUMP_CREATOR_WINDOWS
+  std::cout << "1. MINI_DUMP_NORMAL\n";
+  std::cout << "2. MINI_DUMP_WITHOUT_OPTIONAL_DATA\n";
+  std::cout << "3. MINI_DUMP_IGNORE_INACCESSIBLE_MEMORY\n";
+  std::cout << "4. MINI_DUMP_FILTER_MEMORY\n";
+  std::cout << "5. MINI_DUMP_SCAN_MEMORY\n";
+  std::cout << "6. MINI_DUMP_FILTER_MODULE_PATHS\n";
+  std::cout << "7. MINI_DUMP_WITHOUT_AUXILIARY_STATE\n";
 
-//   std::cout << "\n=== Medium Mini Dumps (128-512KB) ===\n";
-//   std::cout << "8. MINI_DUMP_WITH_DATA_SEGS\n";
-//   std::cout << "9. MINI_DUMP_WITH_HANDLE_DATA\n";
-//   std::cout << "10. MINI_DUMP_WITH_UNLOADED_MODULES\n";
-//   std::cout << "11. MINI_DUMP_WITH_THREAD_INFO\n";
-//   std::cout << "12. MINI_DUMP_WITH_CODE_SEGMENTS\n";
-//   std::cout << "13. MINI_DUMP_WITH_TOKEN_INFORMATION\n";
+  std::cout << "\n=== Medium Mini Dumps (128-512KB) ===\n";
+  std::cout << "8. MINI_DUMP_WITH_DATA_SEGS\n";
+  std::cout << "9. MINI_DUMP_WITH_HANDLE_DATA\n";
+  std::cout << "10. MINI_DUMP_WITH_UNLOADED_MODULES\n";
+  std::cout << "11. MINI_DUMP_WITH_THREAD_INFO\n";
+  std::cout << "12. MINI_DUMP_WITH_CODE_SEGMENTS\n";
+  std::cout << "13. MINI_DUMP_WITH_TOKEN_INFORMATION\n";
 
-//   std::cout << "\n=== Large Mini Dumps (1MB+) ===\n";
-//   std::cout << "14. MINI_DUMP_WITH_PROCESS_THREAD_DATA\n";
-//   std::cout << "15. MINI_DUMP_WITH_FULL_AUXILIARY_STATE\n";
+  std::cout << "\n=== Large Mini Dumps (1MB+) ===\n";
+  std::cout << "14. MINI_DUMP_WITH_PROCESS_THREAD_DATA\n";
+  std::cout << "15. MINI_DUMP_WITH_FULL_AUXILIARY_STATE\n";
 
-//   std::cout << "\n=== Full Memory Dumps (Variable Size) ===\n";
-//   std::cout << "16. MINI_DUMP_WITH_FULL_MEMORY\n";
-//   std::cout << "17. MINI_DUMP_WITH_FULL_MEMORY_INFO\n";
-//   std::cout << "18. MINI_DUMP_WITH_INDIRECTLY_REFERENCED_MEMORY\n";
-//   std::cout << "19. MINI_DUMP_WITH_PRIVATE_READ_WRITE_MEMORY\n";
-//   std::cout << "20. MINI_DUMP_WITH_PRIVATE_WRITE_COPY_MEMORY\n";
+  std::cout << "\n=== Full Memory Dumps (Variable Size) ===\n";
+  std::cout << "16. MINI_DUMP_WITH_FULL_MEMORY\n";
+  std::cout << "17. MINI_DUMP_WITH_FULL_MEMORY_INFO\n";
+  std::cout << "18. MINI_DUMP_WITH_INDIRECTLY_REFERENCED_MEMORY\n";
+  std::cout << "19. MINI_DUMP_WITH_PRIVATE_READ_WRITE_MEMORY\n";
+  std::cout << "20. MINI_DUMP_WITH_PRIVATE_WRITE_COPY_MEMORY\n";
 
-//   std::cout << "\n=== Kernel Dumps ===\n";
-//   std::cout << "21. KERNEL_FULL_DUMP\n";
-//   std::cout << "22. KERNEL_KERNEL_DUMP\n";
-//   std::cout << "23. KERNEL_SMALL_DUMP\n";
-//   std::cout << "24. KERNEL_AUTOMATIC_DUMP\n";
-//   std::cout << "25. KERNEL_ACTIVE_DUMP\n";
-// #else
-//   std::cout << "1. CORE_DUMP_FULL\n";
-//   std::cout << "2. CORE_DUMP_KERNEL_ONLY\n";
-//   std::cout << "3. CORE_DUMP_USER_ONLY\n";
-//   std::cout << "4. CORE_DUMP_COMPRESSED\n";
-//   std::cout << "5. CORE_DUMP_FILTERED\n";
-// #endif
+  std::cout << "\n=== Kernel Dumps ===\n";
+  std::cout << "21. KERNEL_FULL_DUMP\n";
+  std::cout << "22. KERNEL_KERNEL_DUMP\n";
+  std::cout << "23. KERNEL_SMALL_DUMP\n";
+  std::cout << "24. KERNEL_AUTOMATIC_DUMP\n";
+  std::cout << "25. KERNEL_ACTIVE_DUMP\n";
+#else
+  std::cout << "1. CORE_DUMP_FULL\n";
+  std::cout << "2. CORE_DUMP_KERNEL_ONLY\n";
+  std::cout << "3. CORE_DUMP_USER_ONLY\n";
+  std::cout << "4. CORE_DUMP_COMPRESSED\n";
+  std::cout << "5. CORE_DUMP_FILTERED\n";
+#endif
 
-//   std::cout << "0. Exit\n";
-//   std::cout << "\nSelect option: ";
+  std::cout << "0. Exit\n";
+  std::cout << "\nSelect option: ";
 
-//   int choice = 0;
-//   std::cin >> choice;
+  int choice = 0;
+  std::cin >> choice;
 
-//   if(choice == 0)
-//   {
-//     std::cout << "Exiting...\n";
-//     return 0;
-//   }
+  if(choice == 0)
+  {
+    std::cout << "Exiting...\n";
+    return 0;
+  }
 
-// #if DUMP_CREATOR_WINDOWS
-//   const int maxChoice = 25;
-// #else
-//   const int maxChoice = 5;
-// #endif
+#if DUMP_CREATOR_WINDOWS
+  const int maxChoice = 25;
+#else
+  const int maxChoice = 5;
+#endif
 
-//   if(choice >= 1 && choice <= maxChoice)
-//   {
-//     std::vector<DumpType> dumpTypes = {
-// #if DUMP_CREATOR_WINDOWS
-//       DumpType::MINI_DUMP_NORMAL,                            // 1
-//       DumpType::MINI_DUMP_WITHOUT_OPTIONAL_DATA,             // 2
-//       DumpType::MINI_DUMP_IGNORE_INACCESSIBLE_MEMORY,        // 3
-//       DumpType::MINI_DUMP_FILTER_MEMORY,                     // 4
-//       DumpType::MINI_DUMP_SCAN_MEMORY,                       // 5
-//       DumpType::MINI_DUMP_FILTER_MODULE_PATHS,               // 6
-//       DumpType::MINI_DUMP_WITHOUT_AUXILIARY_STATE,           // 7
-//       DumpType::MINI_DUMP_WITH_DATA_SEGS,                    // 8
-//       DumpType::MINI_DUMP_WITH_HANDLE_DATA,                  // 9
-//       DumpType::MINI_DUMP_WITH_UNLOADED_MODULES,             // 10
-//       DumpType::MINI_DUMP_WITH_THREAD_INFO,                  // 11
-//       DumpType::MINI_DUMP_WITH_CODE_SEGMENTS,                // 12
-//       DumpType::MINI_DUMP_WITH_TOKEN_INFORMATION,            // 13
-//       DumpType::MINI_DUMP_WITH_PROCESS_THREAD_DATA,          // 14
-//       DumpType::MINI_DUMP_WITH_FULL_AUXILIARY_STATE,         // 15
-//       DumpType::MINI_DUMP_WITH_FULL_MEMORY,                  // 16
-//       DumpType::MINI_DUMP_WITH_FULL_MEMORY_INFO,             // 17
-//       DumpType::MINI_DUMP_WITH_INDIRECTLY_REFERENCED_MEMORY, // 18
-//       DumpType::MINI_DUMP_WITH_PRIVATE_READ_WRITE_MEMORY,    // 19
-//       DumpType::MINI_DUMP_WITH_PRIVATE_WRITE_COPY_MEMORY,    // 20
-//       DumpType::KERNEL_FULL_DUMP,                            // 21
-//       DumpType::KERNEL_KERNEL_DUMP,                          // 22
-//       DumpType::KERNEL_SMALL_DUMP,                           // 23
-//       DumpType::KERNEL_AUTOMATIC_DUMP,                       // 24
-//       DumpType::KERNEL_ACTIVE_DUMP                           // 25
-// #else
-//       DumpType::CORE_DUMP_FULL // 1
-// #endif
-//     };
+  if(choice >= 1 && choice <= maxChoice)
+  {
+    std::vector<DumpType> dumpTypes = {
+#if DUMP_CREATOR_WINDOWS
+      DumpType::MINI_DUMP_NORMAL,                            // 1
+      DumpType::MINI_DUMP_WITHOUT_OPTIONAL_DATA,             // 2
+      DumpType::MINI_DUMP_IGNORE_INACCESSIBLE_MEMORY,        // 3
+      DumpType::MINI_DUMP_FILTER_MEMORY,                     // 4
+      DumpType::MINI_DUMP_SCAN_MEMORY,                       // 5
+      DumpType::MINI_DUMP_FILTER_MODULE_PATHS,               // 6
+      DumpType::MINI_DUMP_WITHOUT_AUXILIARY_STATE,           // 7
+      DumpType::MINI_DUMP_WITH_DATA_SEGS,                    // 8
+      DumpType::MINI_DUMP_WITH_HANDLE_DATA,                  // 9
+      DumpType::MINI_DUMP_WITH_UNLOADED_MODULES,             // 10
+      DumpType::MINI_DUMP_WITH_THREAD_INFO,                  // 11
+      DumpType::MINI_DUMP_WITH_CODE_SEGMENTS,                // 12
+      DumpType::MINI_DUMP_WITH_TOKEN_INFORMATION,            // 13
+      DumpType::MINI_DUMP_WITH_PROCESS_THREAD_DATA,          // 14
+      DumpType::MINI_DUMP_WITH_FULL_AUXILIARY_STATE,         // 15
+      DumpType::MINI_DUMP_WITH_FULL_MEMORY,                  // 16
+      DumpType::MINI_DUMP_WITH_FULL_MEMORY_INFO,             // 17
+      DumpType::MINI_DUMP_WITH_INDIRECTLY_REFERENCED_MEMORY, // 18
+      DumpType::MINI_DUMP_WITH_PRIVATE_READ_WRITE_MEMORY,    // 19
+      DumpType::MINI_DUMP_WITH_PRIVATE_WRITE_COPY_MEMORY,    // 20
+      DumpType::KERNEL_FULL_DUMP,                            // 21
+      DumpType::KERNEL_KERNEL_DUMP,                          // 22
+      DumpType::KERNEL_SMALL_DUMP,                           // 23
+      DumpType::KERNEL_AUTOMATIC_DUMP,                       // 24
+      DumpType::KERNEL_ACTIVE_DUMP                           // 25
+#else
+      DumpType::CORE_DUMP_FULL // 1
+#endif
+    };
 
-//     CoreDumpGenerator::setDumpType(dumpTypes[choice - 1]);
-//     std::cout << "Selected dump type: " << DumpFactory::getDescription(CoreDumpGenerator::getCurrentDumpType()) << "\n";
-//     std::cout << "Estimated size: " << DumpFactory::getEstimatedSize(CoreDumpGenerator::getCurrentDumpType())
-//               << " bytes\n";
-//     std::cout << "Triggering crash in 3 seconds...\n";
-//     std::this_thread::sleep_for(std::chrono::seconds(3));
+    CoreDumpGenerator::setDumpType(dumpTypes[choice - 1]);
+    std::cout << "Selected dump type: " << DumpFactory::getDescription(CoreDumpGenerator::getCurrentDumpType()) << "\n";
+    std::cout << "Estimated size: " << DumpFactory::getEstimatedSize(CoreDumpGenerator::getCurrentDumpType())
+              << " bytes\n";
+    std::cout << "Triggering crash in 3 seconds...\n";
+    std::this_thread::sleep_for(std::chrono::seconds(3));
 
-//     // Set core pattern for crash (like in the working example)
-// //     CoreDumpGenerator::setCorePatternForCrash();
+    // Set core pattern for crash (like in the working example)
+    CoreDumpGenerator::setCorePatternForCrash();
 
-//     // Trigger crash
-//     // char *null_pointer = nullptr;
-//     // *null_pointer      = CRASH_VALUE; // This will crash
+    // Trigger crash
+    // char *null_pointer = nullptr;
+    // *null_pointer      = CRASH_VALUE; // This will crash
 
-//     foo();
-//   }
+    foo();
+  }
 
-foo();
+  foo();
   std::cout << "This should not be printed!\n";
   return 0;
 }
